f17b622c4ef50e102d6f518b2422b153d9714ef6
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-09 19:56:37 +0600

    added login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -8,8 +8,36 @@ const Login = () => {
     useTitle('Log in');
 
     return (
-        <div>
-
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-5xl text-center font-bold">Login</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="Password" className="input input-bordered" />
+                            <label className="label">
+                                <Link to="/signup" className="label-text-alt link link-hover text-primary-focus">Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center'>Create an account? <Link className='text-primary link-hover font-bold' to="/signup">Sign Up</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
