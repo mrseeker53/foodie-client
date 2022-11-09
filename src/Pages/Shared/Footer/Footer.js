0d843e0e17d622ee0e6178143c300f43af2330e8
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-09 15:55:55 +0600

    added footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import logo from '../../../assets/images/logo.png';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>footer</h2>
-        </div>
+        <footer className="footer p-32 bg-black text-white">
+            <div>
+                <img src={logo} alt="" />
+                <p>Copyright © 2022 <br /> All rights reserved</p>
+            </div>
+            <div>
+                <span className="footer-title">Connect</span>
+                <a href="/" className="link link-hover flex">
+                    <FaFacebook className='mr-2 mt-0.5' /> Facebook
+                </a>
+                <a href="/" className="link link-hover flex">
+                    <FaInstagram className='mr-2 mt-0.5' /> Instagram
+                </a>
+                <a href="/" className="link link-hover flex">
+                    <FaYoutube className='mr-2 mt-0.5' /> Youtube
+                </a>
+                <a href="/" className="link link-hover flex">
+                    <FaWhatsapp className='mr-2 mt-0.5' /> Whatsapp
+                </a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a href="/" className="link link-hover">About</a>
+                <a href="/" className="link link-hover">Services</a>
+                <a href="/" className="link link-hover">Contact</a>
+                <a href="/" className="link link-hover">FAQ</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a href="/" className="link link-hover">Terms of use</a>
+                <a href="/" className="link link-hover">Privacy policy</a>
+                <a href="/" className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
