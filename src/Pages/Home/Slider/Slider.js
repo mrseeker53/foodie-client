1d7e4ac834782fa83dd6abdecad0b81447033c96
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-10 12:58:04 +0600

    added slider

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SliderItem from './SliderItem';
 
 const Slider = () => {
+    const [slider, setSlider] = useState([]);
+
+    // Load data from server
+    useEffect(() => {
+        fetch('https://foodie-server-one.vercel.app/slider')
+            .then(res => res.json())
+            .then(data => setSlider(data));
+    }, []);
+
     return (
-        <div>
-            <h2>Slider</h2>
+        <div className="carousel w-full py-10">
+            {
+                slider.map(slide => <SliderItem
+                    key={slide._id}
+                    slide={slide}
+                ></SliderItem>)
+            }
         </div>
     );
 };
