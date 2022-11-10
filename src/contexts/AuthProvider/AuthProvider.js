4c72596cd4e092af96ef8e2156d4e1647a608c8d
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-10 21:01:22 +0600

    create private route & auth context

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,63 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+
+
+
+// Create & export Context
+export const AuthContext = createContext();
+// Create Auth
+const auth = getAuth(app);
+
+
+const AuthProvider = ({ children }) => {
+    // Declare state
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Create user by email-password
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Create login
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // Create logout
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    // Declare authInfo
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        login,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
