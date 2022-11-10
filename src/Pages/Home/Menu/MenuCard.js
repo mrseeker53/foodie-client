1ab438e1bb60f4a2979e003f23099dd5d0445b77
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-10 18:36:42 +0600

    added photo viewer

--- src/Pages/Home/Menu/MenuCard.js
+++ src/Pages/Home/Menu/MenuCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 // Destructure menu as object
 const MenuCard = ({ menu }) => {
@@ -9,12 +11,16 @@ const MenuCard = ({ menu }) => {
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <figure><img src={img} alt="" /></figure>
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-xl font-semibold'>Price: ${price}</p>
                 <p className='text-xl'>Rating: {rating}</p>
-                <p className='text-lg'>{description}</p>
+                <p className='text-lg'>{description.slice(0, 100)}</p>
                 <div className="card-actions justify-end mt-8">
                     <Link to={`/menu/${_id}`}>
                         <button className="btn btn-outline btn-primary text-white">View Details</button>
